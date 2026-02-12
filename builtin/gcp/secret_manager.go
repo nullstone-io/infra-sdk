4a62bd750a121494fed8ce6cb29de074d01ea126
Brad Sickles <brad.sickles@gmail.com>
2026-02-12 12:19:48 -0500

    Implement gcp secret manager

--- builtin/gcp/secret_manager.go
+++ builtin/gcp/secret_manager.go
@@ -2,9 +2,18 @@ package gcp
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strings"
 
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
 	infra_sdk "github.com/nullstone-io/infra-sdk"
 	"github.com/nullstone-io/infra-sdk/access/gcp"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"gopkg.in/nullstone-io/go-api-client.v0/types"
 )
 
@@ -19,16 +28,146 @@ type SecretManager struct {
 }
 
 func (s SecretManager) List(ctx context.Context, location types.SecretLocation) ([]types.Secret, error) {
-	//TODO implement me
-	panic("implement me")
+	if s.ProviderConfig == nil || s.ProviderConfig.ProviderName == "" {
+		return nil, nil
+	}
+	client, err := s.smClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	parent := fmt.Sprintf("projects/%s", location.GcpProjectId)
+	it := client.ListSecrets(ctx, &secretmanagerpb.ListSecretsRequest{Parent: parent})
+
+	result := make([]types.Secret, 0)
+	for {
+		secret, err := it.Next()
+		if errors.Is(err, iterator.Done) {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("error listing secrets: %w", err)
+		}
+		result = append(result, types.Secret{
+			Identity: s.secretIdentityFromGcp(secret.Name),
+			Metadata: map[string]any{
+				"labels": secret.Labels,
+			},
+			Value:    "",
+			Redacted: true,
+		})
+	}
+	return result, nil
 }
 
 func (s SecretManager) Create(ctx context.Context, identity types.SecretIdentity, value string) (*types.Secret, error) {
-	//TODO implement me
-	panic("implement me")
+	if s.ProviderConfig == nil || s.ProviderConfig.ProviderName == "" {
+		return nil, nil
+	}
+	client, err := s.smClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	secret, err := client.CreateSecret(ctx, &secretmanagerpb.CreateSecretRequest{
+		Parent:   fmt.Sprintf("projects/%s", identity.GcpProjectId),
+		SecretId: identity.Name,
+		Secret: &secretmanagerpb.Secret{
+			Replication: &secretmanagerpb.Replication{
+				Replication: &secretmanagerpb.Replication_Automatic_{
+					Automatic: &secretmanagerpb.Replication_Automatic{},
+				},
+			},
+		},
+	})
+	if err != nil {
+		if status.Code(err) == codes.AlreadyExists {
+			return nil, infra_sdk.ErrSecretAlreadyExists
+		}
+		return nil, fmt.Errorf("error creating secret: %w", err)
+	}
+
+	_, err = client.AddSecretVersion(ctx, &secretmanagerpb.AddSecretVersionRequest{
+		Parent: secret.Name,
+		Payload: &secretmanagerpb.SecretPayload{
+			Data: []byte(value),
+		},
+	})
+	if err != nil {
+		return nil, fmt.Errorf("error adding secret version: %w", err)
+	}
+
+	return &types.Secret{
+		Identity: identity,
+		Metadata: nil,
+		Value:    "",
+		Redacted: false,
+	}, nil
 }
 
 func (s SecretManager) Update(ctx context.Context, identity types.SecretIdentity, value string) (*types.Secret, error) {
-	//TODO implement me
-	panic("implement me")
+	if s.ProviderConfig == nil || s.ProviderConfig.ProviderName == "" {
+		return nil, nil
+	}
+	client, err := s.smClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer client.Close()
+
+	_, err = client.AddSecretVersion(ctx, &secretmanagerpb.AddSecretVersionRequest{
+		Parent: fmt.Sprintf("projects/%s", identity.GcpProjectId),
+		Payload: &secretmanagerpb.SecretPayload{
+			Data: []byte(value),
+		},
+	})
+	if err != nil {
+		if status.Code(err) == codes.NotFound {
+			return nil, infra_sdk.ErrDoesNotExist
+		}
+		return nil, fmt.Errorf("error updating secret: %w", err)
+	}
+
+	return &types.Secret{
+		Identity: identity,
+		Metadata: nil,
+		Value:    "",
+		Redacted: false,
+	}, nil
+}
+
+func (s SecretManager) smClient(ctx context.Context) (*secretmanager.Client, error) {
+	tokenSource, err := gcp.ResolveTokenSource(ctx, s.Assumer, s.Provider)
+	if err != nil {
+		return nil, fmt.Errorf("error resolving gcp credentials: %w", err)
+	}
+
+	client, err := secretmanager.NewClient(ctx, option.WithTokenSource(tokenSource))
+	if err != nil {
+		return nil, fmt.Errorf("error creating gcp secret manager client: %w", err)
+	}
+	return client, nil
+}
+
+func (s SecretManager) secretIdentityFromGcp(secretName string) types.SecretIdentity {
+	identity := types.SecretIdentity{
+		SecretLocation: types.SecretLocation{
+			Platform:     types.SecretLocationPlatformGcp,
+			GcpProjectId: s.Provider.ProviderId,
+		},
+	}
+	// secretName is one of:
+	// - "projects/{project}/secrets/{secretId}"
+	// - "projects/{project}/locations/{location}/secrets/{secretId}"
+	parts := strings.Split(secretName, "/")
+	if len(parts) == 4 {
+		identity.GcpProjectId = parts[1]
+		identity.Name = parts[len(parts)-1]
+	} else if len(parts) == 6 {
+		identity.GcpProjectId = parts[1]
+		identity.Name = parts[len(parts)-1]
+	}
+	return identity
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/nullstone-io/infra-sdk
 go 1.24.0
 
 require (
+	cloud.google.com/go/secretmanager v1.16.0
 	github.com/aws/aws-sdk-go-v2 v1.41.1
 	github.com/aws/aws-sdk-go-v2/credentials v1.18.19
 	github.com/aws/aws-sdk-go-v2/service/apigateway v1.35.9
@@ -27,13 +28,18 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sqs v1.42.11
 	github.com/aws/aws-sdk-go-v2/service/sts v1.38.9
 	github.com/stretchr/testify v1.11.1
-	golang.org/x/net v0.48.0
+	golang.org/x/net v0.49.0
 	golang.org/x/oauth2 v0.35.0
+	google.golang.org/api v0.266.0
+	google.golang.org/grpc v1.78.0
 	gopkg.in/nullstone-io/go-api-client.v0 v0.0.0-20260211232559-c209a9da78a0
 )
 
 require (
-	cloud.google.com/go/compute/metadata v0.3.0 // indirect
+	cloud.google.com/go/auth v0.18.1 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
+	cloud.google.com/go/compute/metadata v0.9.0 // indirect
+	cloud.google.com/go/iam v1.5.3 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.7.2 // indirect
@@ -45,23 +51,40 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.13.11 // indirect
 	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.19.11 // indirect
 	github.com/aws/smithy-go v1.24.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.11 // indirect
+	github.com/googleapis/gax-go/v2 v2.17.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.24.0 // indirect
 	github.com/jinzhu/copier v0.4.0 // indirect
-	github.com/kr/pretty v0.3.1 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/nullstone-io/module v0.2.10 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/tmccombs/hcl2json v0.6.8 // indirect
 	github.com/zclconf/go-cty v1.17.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.61.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.64.0 // indirect
+	go.opentelemetry.io/otel v1.39.0 // indirect
+	go.opentelemetry.io/otel/metric v1.39.0 // indirect
+	go.opentelemetry.io/otel/trace v1.39.0 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
 	golang.org/x/mod v0.32.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/text v0.32.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	golang.org/x/time v0.14.0 // indirect
 	golang.org/x/tools v0.40.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	google.golang.org/genproto v0.0.0-20260128011058-8636f8732409 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20260128011058-8636f8732409 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260203192932-546029d2fa20 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
